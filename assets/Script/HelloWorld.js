d38e86232e5bd6c2fab5990e042d372a747f478d
yunteng <maweiphilippines@gmail.com>
2021-05-20 18:01:46 +0800

    获取shareTrace 结果成功

--- assets/Script/HelloWorld.js
+++ assets/Script/HelloWorld.js
@@ -21,8 +21,17 @@ cc.Class({
     },
     onBtnClick(event,param){
         cc.log("==点击按钮");
-        if  (cc.sys.OS_IOS === cc.sys.os){
-            jsb.reflection.callStaticMethod("NativeOcClass","callNativeUIWithTitle:title andContent:","cocos2d-js","")
+        if (param === "test"){
+            if  (cc.sys.OS_IOS === cc.sys.os){
+                alert(cc.sys.os)
+                var ret = jsb.reflection.callStaticMethod("NativeOcClass",
+                    "callNativeUIWithTitle:andContent:",
+                    "123木头人",
+                    "我们的故事，是很长的电影");
+            }
+        }else if (param === "sharetrace"){
+            var ret = jsb.reflection.callStaticMethod("SDKManager","getSharetraceParam");
+            cc.log("====sharetrace结果:",ret);
         }
     }
 });
--- build/jsb-default/js backups (useful for debugging)/project.js
+++ build/jsb-default/js backups (useful for debugging)/project.js
@@ -1,32 +1,32 @@
-window.__require = function e(r, t, n) {
-function c(l, u) {
+window.__require = function e(t, c, n) {
+function o(l, i) {
+if (!c[l]) {
 if (!t[l]) {
-if (!r[l]) {
-var i = l.split("/");
-i = i[i.length - 1];
-if (!r[i]) {
-var _ = "function" == typeof __require && __require;
-if (!u && _) return _(i, !0);
-if (o) return o(i, !0);
+var u = l.split("/");
+u = u[u.length - 1];
+if (!t[u]) {
+var s = "function" == typeof __require && __require;
+if (!i && s) return s(u, !0);
+if (r) return r(u, !0);
 throw new Error("Cannot find module '" + l + "'");
 }
-l = i;
+l = u;
 }
-var s = t[l] = {
+var _ = c[l] = {
 exports: {}
 };
-r[l][0].call(s.exports, function(e) {
-return c(r[l][1][e] || e);
-}, s, s.exports, e, r, t, n);
+t[l][0].call(_.exports, function(e) {
+return o(t[l][1][e] || e);
+}, _, _.exports, e, t, c, n);
 }
-return t[l].exports;
+return c[l].exports;
 }
-for (var o = "function" == typeof __require && __require, l = 0; l < n.length; l++) c(n[l]);
-return c;
+for (var r = "function" == typeof __require && __require, l = 0; l < n.length; l++) o(n[l]);
+return o;
 }({
-HelloWorld: [ function(e, r, t) {
+HelloWorld: [ function(e, t, c) {
 "use strict";
-cc._RF.push(r, "280c3rsZJJKnZ9RqbALVwtK", "HelloWorld");
+cc._RF.push(t, "280c3rsZJJKnZ9RqbALVwtK", "HelloWorld");
 cc.Class({
 extends: cc.Component,
 properties: {
@@ -37,13 +37,17 @@ type: cc.Label
 text: "Hello, World!"
 },
 onLoad: function() {},
-update: function(e) {}
+update: function(e) {},
+onBtnClick: function(e, t) {
+cc.log("==点击按钮");
+cc.sys.OS_IOS === cc.sys.os && jsb.reflection.callStaticMethod("NativeOcClass", "callNativeUIWithTitle:title andContent:", "cocos2d-js", "");
+}
 });
 cc._RF.pop();
 }, {} ],
-"use_v2.1-2.2.1_cc.Toggle_event": [ function(e, r, t) {
+"use_v2.1-2.2.1_cc.Toggle_event": [ function(e, t, c) {
 "use strict";
-cc._RF.push(r, "84bc1D1nVlDMbvSqc1JZyQ2", "use_v2.1-2.2.1_cc.Toggle_event");
+cc._RF.push(t, "84bc1D1nVlDMbvSqc1JZyQ2", "use_v2.1-2.2.1_cc.Toggle_event");
 cc.Toggle && (cc.Toggle._triggerEventInScript_isChecked = !0);
 cc._RF.pop();
 }, {} ]
