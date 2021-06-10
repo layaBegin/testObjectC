8e611bfdad8ba1481a9bdc402d82f0fe64608012
yunteng <maweiphilippines@gmail.com>
2021-06-10 18:04:15 +0800

    ignore something

--- build/jsb-default/js backups (useful for debugging)/project.js
+++ build/jsb-default/js backups (useful for debugging)/project.js
@@ -27,6 +27,11 @@ return o;
 HelloWorld: [ function(e, t, c) {
 "use strict";
 cc._RF.push(t, "280c3rsZJJKnZ9RqbALVwtK", "HelloWorld");
+var n = function(e) {
+return e && e.__esModule ? e : {
+default: e
+};
+}(e("./UIManager"));
 cc.Class({
 extends: cc.Component,
 properties: {
@@ -34,40 +39,56 @@ label: {
 default: null,
 type: cc.Label
 },
-text: "Hello, World!"
+label1: {
+default: null,
+type: cc.Label
+}
 },
-onLoad: function() {},
+onLoad: function() {
+window.callOCMethoed = function(e) {
+console.log(e);
+};
+},
+callOCMethoed: function(e) {
+console.log(e);
+alert("====" + e);
+},
 update: function(e) {},
 onBtnClick: function(e, t) {
 cc.log("==点击按钮");
 if ("test" === t) {
 if (cc.sys.OS_IOS === cc.sys.os) {
 alert(cc.sys.os);
-var c = jsb.reflection.callStaticMethod("NativeOcClass", "callNativeUIWithTitle:andContent:", "123木头人", "我们的故事，是很长的电影");
+jsb.reflection.callStaticMethod("NativeOcClass", "callNativeUIWithTitle:andContent:", "123木头人", "我们的故事，是很长的电影");
 }
 } else if ("sharetrace" === t) {
-c = jsb.reflection.callStaticMethod("SDKManager", "getSharetraceParam");
+var c = jsb.reflection.callStaticMethod("SDKManager", "getSharetraceParam");
 cc.log("====sharetrace结果:", c);
+this.label1.string = c;
+n.default.showTipsUI(c);
 }
 }
 });
 cc._RF.pop();
-}, {} ],
+}, {
+"./UIManager": "UIManager"
+} ],
 SDKCallBack: [ function(e, t, c) {
 "use strict";
 cc._RF.push(t, "9b9e4xxmT1GYLniHr/X6wZ1", "SDKCallBack");
 Object.defineProperty(c, "__esModule", {
 value: !0
 });
-var n = function() {
+var n = new (function() {
 function e() {}
 e.prototype.testFromOC = function(e) {
 cc.log("==从原生执行js代码成功:", e);
 alert("==从原生执行js代码成功" + e);
 };
 return e;
-}();
-c.default = new n();
+}())();
+window.SDKCallBack = n;
+c.default = n;
 cc._RF.pop();
 }, {} ],
 TipsUI: [ function(e, t, c) {
@@ -106,7 +127,9 @@ var n = function() {
 function e() {}
 e.prototype.showTipsUI = function(e) {
 cc.loader.loadRes("TipsUI", cc.Prefab, function(t, c) {
+cc.log("==进入生成tips回掉");
 if (!t) {
+cc.log("==tips回掉成功");
 var n = cc.instantiate(c);
 n.parent = cc.Canvas.instance.node;
 n.getComponent("TipsUI").updateUI(e);
--- temp/quick-scripts/src/__qc_index__.js
+++
@@ -1,6 +0,0 @@
-
-require('./assets/Script/HelloWorld');
-require('./assets/Script/SDKCallBack');
-require('./assets/Script/TipsUI');
-require('./assets/Script/UIManager');
-require('./assets/migration/use_v2.1-2.2.1_cc.Toggle_event');
